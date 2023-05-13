91b19952465b63cef8c9a3931a6a39d3df075862
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2023-05-14 00:13:44 +0100

    using the filter method to find the difference between two arrays returned as an array

--- algorithms.js
+++ algorithms.js
@@ -434,3 +434,19 @@ function sumAll(arr) {
  }
  
  console.log(sumAll([1, 4]));
+
+
+//  Diff Two Arrays
+// Compare two arrays and return a new array with any items only found in one of the two given arrays, 
+// but not both. In other words, return the symmetric difference of the two arrays.
+// Note: You can return the array with its elements in any order.
+
+function diffArray(arr1, arr2) {
+
+  let concatArr = arr1.concat(arr2)
+
+return concatArr.filter((item) => !arr1.includes(item) || !arr2.includes(item))
+
+
+}
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
